fc49a5a72caebc382099b2a6274769f6545a1daa
Oberhofer <119164390+ulrich-oberhofer@users.noreply.github.com>
2022-12-14 11:02:27 +0100

    Update Functions_and_different_models.py

--- Functions_and_different_models.py
+++ Functions_and_different_models.py
@@ -11,10 +11,13 @@ import sdeint
 #!pip install kramersmoyal
 from kramersmoyal import km
 from sklearn.neighbors import KernelDensity
-
 from scipy.stats import entropy
 
-#Necessary functions: De-trending, Estimation of KM-Coefficients, Calculation of Power mismatch, Calculation of c_2
+'''For calculations: use angular velocity omega = 2*pi*frequency
+
+
+
+'''Necessary functions: De-trending, Estimation of KM-Coefficients, Calculation of Power mismatch, Calculation of c_2'''
 '''Different models: 1: linear c_1 and epsilon, no c_2 and Delta_P
                      2: linear c_1,c_2 and epsilon,
                      3: cubic c_1(omega), state-dependent c_2 and epsilon
