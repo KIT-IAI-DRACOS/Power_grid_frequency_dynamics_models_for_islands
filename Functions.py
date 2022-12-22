66c38b5077bb7fd20cdd78d8d284816cfa7b7908
Oberhofer <119164390+ulrich-oberhofer@users.noreply.github.com>
2022-12-22 14:23:18 +0100

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -307,7 +307,11 @@ def Euler_Maruyama(data,delta_t,t_final,model,c_1,c_2,Delta_P,epsilon,factor_dai
       else:
         P[i] = 1/3     
     elif model == 3:
-      Delta_P_fun[i] = Delta_P[(i//(int(4/dispatch)*900*mul))%(dispatch*24)] #define "power"-function (continuous Delta_P)
+      if i%(900*mul*int(4/change)) <60:   #TAKE 60 seconds!!!
+        P_slow = i%(900/delta_t*int(4/dispatch))/60
+      else:
+         P_slow = 1
+      Delta_P_fun[i] = 1* P_slow * Delta_P[(i//(int(4/dispatch)*900/delta_t))%(dispatch*24)]  +(1-P_slow[i])*  Delta_P_new[((i//(int(4/dispatch)*900/delta_t))-1)%(dispatch*24)]
   
   for i in range(1,time.size):
     theta[i] = theta[i-1] + delta_t * omega[i-1]
