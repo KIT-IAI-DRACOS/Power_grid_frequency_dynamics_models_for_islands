77a2b0c6f6feaba9bd4801e0cbdba9674d1d610c
Oberhofer <119164390+ulrich-oberhofer@users.noreply.github.com>
2023-01-04 18:11:30 +0100

    Update and rename Read_data.py to Read_data.md

---
+++ Data/Read_data.md
@@ -0,0 +1,2 @@
+Uploud of a snippet of data, probably for one grid'''
+To receive the complete data set or for questions regarding the code, please write a mail to ulrich.oberhofer@kit.edu'''
--- Data/Read_data.py
+++
@@ -1,2 +0,0 @@
-'''Uploud of a snippet of data, probably for one grid'''
-'''To receive the complete data set or for questions regarding the code, please write a mail to ulrich.oberhofer@kit.edu'''
