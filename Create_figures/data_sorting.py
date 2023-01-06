d207fd2304b3e5b92eaf9462308e0d29d682a8df
Oberhofer <119164390+ulrich-oberhofer@users.noreply.github.com>
2023-01-06 18:35:06 +0100

    Update data_sorting.py

--- Create_figures/data_sorting.py
+++ Create_figures/data_sorting.py
@@ -1,4 +1,3 @@
-# %%
 import numpy as np
 
 data_sources = ['Iceland_data.npz', 'Irish_data.npz', 'Balearic_data.npz']
@@ -57,25 +56,25 @@ sort_data(data_sources[2], filenames[2])
 
 
 # %% data soruting for Fig2
-data = np.load('Irish_KM_coefficients_new_1d_model.npz')
-data2 = np.load('Irish_KM_coefficients_new_2d_model.npz')
+data = np.load('KM_coefficients.npz')
+
 
 edges_1d = data['edges_1d'],
 drift_1d = data['kmc_1d_drift'],
-diffusion_1d = data['kmc_1s_diffusion'],
-edges_2d = data2['edges_2d'],
-kmc_2d = data2['kmc_2d'],
+diffusion_1d = data['kmc_1d_diffusion'],
+edges_2d = data['edges_2d'],
+kmc_2d = data['kmc_2d'],
 
-c_1_model1 = data['c_1_simple_model'],
-c_1_model2 = data['c_1_drift_old_model'],
+c_1_model1 = data['c_1_model1'],
+c_1_model2 = data['c_1_model2'],
 
-p_3_model3 = data['p_3_drift_new_1d_model'],
-p_1_model3 = data['p_1_drift_new_1d_model'],
+p_3_model3 = data['p_3_model3'],
+p_1_model3 = data['p_1_model3'],
 
-epsilon_model2 = data['epsilon_old_model'],
-epsilon_model1 = data['epsilon_simple_model'],
-d_2_model3 = data['d_2_new_1d_model'],
-d_0_model3 = data['d_0_new_1d_model'],
+epsilon_model2 = data['epsilon_model2'],
+epsilon_model1 = data['epsilon_model1'],
+d_2_model3 = data['d_2_model3'],
+d_0_model3 = data['d_0_model3'],
 
 # %%
 np.savez_compressed('fig2_Ireland',
