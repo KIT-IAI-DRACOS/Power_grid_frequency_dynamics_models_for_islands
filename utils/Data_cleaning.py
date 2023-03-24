88e0a6fb2b6fb091f181a7d1b73ba2ff66a59dd8
Ulrich Oberhofer <ulrich.oberhofer@kit.edu>
2023-03-24 15:47:13 +0100

    Corrected inclusion of function

--- utils/Data_cleaning.py
+++ utils/Data_cleaning.py
@@ -1,5 +1,5 @@
 import numpy as np
-from Help_functions import extreme_points, isolated_peaks, const_windows, nan_windows
+from utils.Help_functions import extreme_points, isolated_peaks, const_windows, nan_windows
 
 def data_cleaning(data ):
     # %%  Set parameters for identifying corrupted data
